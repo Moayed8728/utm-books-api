40a263aba7928492ac8a5e7a1dbccabc4f62dc54
Mr.Moayed <mwyd8728@gmail.com>
2026-06-22 15:02:09 +0800

    done

--- src/Middleware/Cors.php
+++ src/Middleware/Cors.php
@@ -8,15 +8,32 @@ use Psr\Http\Server\RequestHandlerInterface;
 
 final class Cors implements MiddlewareInterface
 {
+    private array $allowed;
+
+    public function __construct()
+    {
+        $list = $_ENV['CORS_ALLOWED_ORIGINS'] ?? '';
+        $this->allowed = array_filter(array_map('trim', explode(',', $list)));
+    }
+
     public function process(
         ServerRequestInterface $request,
         RequestHandlerInterface $handler
     ): ResponseInterface {
+        $origin = $request->getHeaderLine('Origin');
+
+        $allowOrigin = '*';
+
+        if (!empty($this->allowed) && in_array($origin, $this->allowed, true)) {
+            $allowOrigin = $origin;
+        }
+
         $response = $handler->handle($request);
 
         return $response
-            ->withHeader('Access-Control-Allow-Origin', 'http://localhost:5173')
+            ->withHeader('Access-Control-Allow-Origin', $allowOrigin)
             ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept')
-            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->withHeader('Vary', 'Origin');
     }
 }
